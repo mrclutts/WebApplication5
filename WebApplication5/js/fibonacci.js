9f61bf7cc193f9be7b85b55efbd1e22b07993b2e
mrclutts <mrclutts@ncsu.edu>
2016-02-12 11:30:26 -0500

    2.12.16 Update
    Added JQuery to modals.

--- WebApplication5/js/fibonacci.js
+++ WebApplication5/js/fibonacci.js
@@ -1,32 +1,37 @@
-function fibonacci() {
-var userInput = parseInt(document.getElementById("fib1").value);
-var vali = validateFib(userInput);
-	if (!vali) {
-		document.getElementById("printfib").innerHTML = "Please enter a positive integer."
-	}
-	else {
-	var a = 1;
-	var b = 0;
-	var c = a + b;
-	var arr = [];
-		for(i=1; c < userInput; ) {		
-			c = a + b;
-			if(c>= userInput)
-				{
-					break;
-				}
-				arr.push(c);
-				document.getElementById("printfib").innerHTML = arr.join(" ");
-				a=b;
-				b=c;
-				
-		}
-	}
-}
+$(document).ready(function () {
+    $("#showfib").on("click", function () {
+        var userInput = parseInt($("#fib1").val());
+        if (isNaN(userInput)){
+            $("#printfib").text("Please enter a number!").show();
+            $("#printfib").addClass("error");
+        }
+        else if (userInput <= 0) {
+            $("#printfib").text("Please enter a positive number!").show();
+            $("#printfib").addClass("error");
+        }
+        else{
+            var a = 1;
+            var b = 0;
+            var c = a + b;
+            var arr = [];
+            for (i = 1; c < userInput;) {
+                c = a + b;
+                if (c >= userInput) {
+                    break;
+                }
+                arr.push(c);
+                $("#printfib").removeClass("error");
+                $("#printfib").text(arr.join(" ")).show();
+                $()
+                a = b;
+                b = c;
+            }
+        }
+    });
+    $("#clearfib").on("click", function () {
+        $("#printfib").hide();
+        $("#fib1").val(" ");
+    });
+});
 
-function validateFib(userInput) {
-	if(userInput <= 0)
-		return false
-	else
-		return true
-}
+
