ee68fbb1e60c3d41f8f519f921c7a821d54d264a
mrclutts <mrclutts@ncsu.edu>
2016-02-26 19:44:40 -0500

    Additional Updates to Portfolio

---
+++ WebApplication5/obj/Release/Package/PackageTmp/js/eightBall.js
@@ -0,0 +1,44 @@
+﻿var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope, $timeout) {
+    $scope.magic = function () {
+        var predict = Math.floor(Math.random() * 9);
+        $scope.result = "";
+        switch (predict) {
+            case 0:
+                result = "I'm sorry, I was out getting tacos.  Ask me again after I get done eating.";
+                break;
+            case 1:
+                result = "My sources say.....YES!";
+                break;
+            case 2:
+                result = "My sources say.....NO!";
+                break;
+            case 3:
+                result = "Would you ask your mother that question?";
+                break;
+            case 4:
+                result = "The answer to your question is a firm maybe.";
+                break;
+            case 5:
+                result = "I've thought abour your query and I have concluded that the answer is YES.";
+                break;
+            case 6:
+                result = "What's with all the quetsions?";
+                break;
+            case 7:
+                result = "Does a bear crap in the woods?";
+                break;
+            case 8:
+                result = "Can pigs fly?";
+                break;
+        }
+        $scope.result = result;
+    }
+    $scope.setDelay = function () {
+        $scope.delay = true;
+        $timeout(function () {
+            $scope.delay = false;
+        }, 1500);
+    }
+});
---
+++ WebApplication5/obj/Release/Package/PackageTmp/js/portfolio.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('.portfolioHolder').children('div:not(.javaScript)').hide();
+    $('#filterOptions li a').click(function () {
+        var ourClass = $(this).attr('class');
+        $('#filterOptions li').removeClass('active');
+        $(this).parent().addClass('active');
+
+        if (ourClass == 'allWork') {
+            $(".portfolioHolder").children(".portfolio-item").hide();
+            $('.portfolioHolder').children(".portfolio-item").each(function(index) {
+                $(this).delay(100*index).fadeIn(2000);
+            });
+            }
+        else {
+            $(".portfolioHolder").children(".portfolio-item").hide();
+            $('.portfolioHolder').children('div:not(.' + ourClass + ')').hide();
+            $('.portfolioHolder').children('div.' + ourClass).each(function(index) {
+                $(this).delay(100*index).fadeIn(1500);
+            });
+        }
+        return false;
+    });
+});
