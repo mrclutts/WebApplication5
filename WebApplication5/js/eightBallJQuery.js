08e997d550ed5dd6765f937db810b97fc7a7be88
mrclutts <mrclutts@ncsu.edu>
2016-02-26 19:41:51 -0500

    Updates to Portfolio
    JQeury filtering tabs.  Added 8Ball and angular updates.

---
+++ WebApplication5/js/eightBallJQuery.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#eightbtn").click(function () {
+        $("#eightball").effect("shake", { times: 5 }, 1500);
+        $("#eightResult").fadeIn(10000);
+    });
+    $("#eightClick").click(function () {
+        $("#eightball").effect("shake", { times: 5 }, 1500);
+        
+    });
+    
+});
