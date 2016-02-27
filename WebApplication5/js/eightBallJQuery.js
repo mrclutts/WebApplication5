7cb8177675891b38d473152a76bb6eb05c7b9637
mrclutts <mrclutts@ncsu.edu>
2016-02-27 15:56:47 -0500

    Updates to Contact and Portfolio
    Background image to contact section, updates to portfolio display.

--- WebApplication5/js/eightBallJQuery.js
+++ WebApplication5/js/eightBallJQuery.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     $("#eightbtn").click(function () {
         $("#eightball").effect("shake", { times: 5 }, 1500);
-        $("#eightResult").fadeIn(10000);
+       
     });
     $("#eightClick").click(function () {
         $("#eightball").effect("shake", { times: 5 }, 1500);
